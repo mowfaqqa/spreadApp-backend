600f8731fdd5ed0b14979d70ad76277085a1c5d7
muwaffaq <itzmuwaffaq@gmail.com>
2023-06-12 20:04:23 +0100

    created endpoint for adding and deleting columns and rows

--- src/controllers/TableController.ts
+++ src/controllers/TableController.ts
@@ -33,3 +33,76 @@ export const deleteTable = async (req: Request, res:Response) => {
     res.status(500).json({ message: 'Server Error' });
   }
 }
+
+export const addRow = async (req: Request, res:Response) => {
+  try {
+    const { id } = req.params
+    const table = await Table.findById(id);
+    if(!table) {
+      return res.status(404).json({ message: "Table not found"});
+    }
+    table.rows.push({});
+    await table.save()
+    res.status(200).json(table);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server Error' });
+  }
+}
+
+export const updateCell = async (req: Request, res: Response) => {
+  try {
+    const { id, rowIndex, column, value } = req.params
+    const table = await Table.findById(id);
+    if (!table) {
+      return res.status(404).json({message: 'Table not found'});
+    }
+    const numberedRowIndex = parseInt(rowIndex)
+    table.rows[numberedRowIndex][column] = value;
+    await table.save();
+    res.status(200).json(table)
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server Error' });
+  }
+};
+
+export const addColumn = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params
+    const { column } = req.body;
+    const table = await Table.findById(id);
+    if (!table) {
+      return res.status(404).json({message : 'Table not found'})
+    }
+    table.columns.push(column);
+    table.rows.forEach((row) => {
+      row[column] = '';
+    });
+    await table.save();
+    res.status(200).json(table);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server Error' });
+  }
+}
+
+export const deleteColumn = async (req: Request, res: Response) => {
+  try {
+    const { id, columnIndex } = req.params;
+    const table = await Table.findById(id);
+    if (!table) {
+      return res.status(404).json({message: 'Table not found'});
+    }
+    const parsedColumnIndex = parseInt(columnIndex)
+    const columnToDelete = table.columns[parsedColumnIndex];
+    table.columns.splice(parsedColumnIndex, 1);
+    table.rows.forEach((row) => {
+      delete row[columnToDelete];
+    });
+    await table.save();
+    res.json(table);
+  } catch (error) {
+    
+  }
+}
